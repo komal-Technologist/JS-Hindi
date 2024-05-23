135f062fe20fd2fad5986749ce3defd9cc0a2720
komal-Technologist <99126724+komal-Technologist@users.noreply.github.com>
2024-05-23 03:45:49 +0000

    commit

---
+++ 01_Basics/datatype.js
@@ -0,0 +1,26 @@
+"use strict" ; //treat all JS code as newer version
+
+//alert (3+ 3)  //we are using node.js not bbrowser
+
+console.log(3+3);  //code readibility should be high
+console.log("Hitesh")
+//docuemnts mdm and https://tc39.es/ecma262/#sec-global-object
+
+ let name = "komal"
+ let age = 22
+ let isLoogedIn = false
+let statate;
+let state = null
+ //number  => 2to power 53
+ //bignint 
+ //string => ""
+ //boolean = true/false
+ //null => standalone value 
+ //undefined=>mtlb abhi value assign ni hui hai 
+ //symbole =>uniq
+
+ //object
+
+ console.log(typeof "hitesh")
+ console.log(typeof null)
+ console.log(typeof undefined)
---
+++ 01_Basics/variables.js
@@ -0,0 +1,18 @@
+const accountno = 21345454
+let accountholdername = "Komal yadav"
+var password = "3232"
+let status;//undefined print hoga
+city = "Lucjnow"//ye tarika bilkul accha nhi hai
+
+
+ //accountno = 76473672
+ accountholdername="goldi"
+ password="343232"
+ city = "Bengluru"
+/*
+dont use var because of block and functional scope 
+*/
+
+
+console.table([accountno,accountholdername,password,city,status])
+
